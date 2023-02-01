0290a7fa1527f93cad556b9ecfbb23cd94b96641
MeetMRP <meetpatelmrp1005@gmail.com>
2023-02-01 21:19:14 +0530

    Missions CRUD finish

--- missions/serializers.py
+++ missions/serializers.py
@@ -1,24 +1,11 @@
 from rest_framework import serializers
 from .models import *
+from ghosts_and_equipments.models import *
 from ghosts_and_equipments.serializers import GhostSerialisers, EquipmentSerialisers
 
 class MissionsSerializers(serializers.ModelSerializer):
-    encountered_ghosts = serializers.SlugRelatedField(queryset=Ghost.objects.all(), slug_field='ghost_name')
-    equipment_used = serializers.SlugRelatedField(queryset=Equipment.objects.all(), slug_field='name')
+    encountered_ghosts = serializers.PrimaryKeyRelatedField(many=True, queryset=Ghost.objects.all())
+    equipment_used = serializers.PrimaryKeyRelatedField(many=True, queryset=Equipment.objects.all())
     class Meta:
         model = Mission
-        fields = ['mission_name', 'mission_date', 'description', 'encountered_ghosts', 'equipment_used', 'is_complete']
-
-    def save(self):
-        encountered_ghosts =self.validated_data['encountered_ghosts']
-        equipment_used = self.validated_data['equipment_used']
-        report = Mission(
-            mission_name=self.validated_data['mission_name'],
-            mission_date=self.validated_data['mission_date'],
-            description=self.validated_data['description'],
-            is_complete=self.validated_data['is_complete'],
-        )
-        report.save()
-        report.encountered_ghosts.set([encountered_ghosts])
-        report.equipment_used.set([equipment_used])
-        report.save()
+        fields = '__all__'
--- missions/views.py
+++ missions/views.py
@@ -1,20 +1,16 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Mission
 from .serializers import MissionsSerializers
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import *
-from django_filters.rest_framework import DjangoFilterBackend
 
 class MissionsApi(ModelViewSet):
     queryset = Mission.objects.all()
     serializer_class = MissionsSerializers
     authentication_classes = [JWTAuthentication]
-    filter_backends = (DjangoFilterBackend,)
-    filter_fields=('encountered_ghosts','equipment_used')
 
     permission_classes_by_action = {
-        'create': [AllowAny],
+        'create': [IsAuthenticated],
         'list': [IsAuthenticated],
         'retrieve': [IsAuthenticated],
         'update': [IsAdminUser],
